883d00d542d44a26384989ff409515cda484386d
Emily Lam <emilymeiyilam@gmail.com>
2023-01-03 21:14:04 +1100

    completed barebones login/navbar

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,3 @@ Rails.application.routes.draw do
   end
 
 end
-
-# Quesitons
-# Do we need a show page for the reviews or is it like a 'pop up' when
-# - how does the reviews page trigger - after they visit the location? or is this too hard and we just give the user a button - location tracking?
-# look at the routes for reviews - does it need its own route to for the reviews page...popup?
