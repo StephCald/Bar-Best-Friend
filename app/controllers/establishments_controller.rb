169d80d2cca1353ca1eea03edbafda9c052cca47
Emily Lam <emilymeiyilam@gmail.com>
2023-01-06 00:15:33 +1100

    fixed formatting, css

---
+++ app/controllers/establishments_controller.rb
@@ -0,0 +1,23 @@
+class EstablishmentsController < ApplicationController
+  def index
+    @user = current_user
+    @establishments = Establishment.all
+  end
+
+  def show
+   @user = current_user
+   @establishment = Establishment.find(params[:id])
+
+  end
+
+  def new
+   @user = current_user
+   @establishment = Establishment.new
+ end
+
+  private
+
+  def establishment_params
+   params.require(:establishment).permit(:name, :phone, :address, :type, :rating, :image)
+  end
+ end
