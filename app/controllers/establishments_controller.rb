7161b401840a794ecd6e2ba5b70e5ec452945400
Emily Lam <emilymeiyilam@gmail.com>
2023-01-05 21:38:25 +1100

    new establisments page created

---
+++ app/controllers/establishments_controller.rb
@@ -0,0 +1,27 @@
+class EstablishmentsController < ApplicationController
+  def index
+    @user = current_user
+    @establishments = Establishment.all
+  end
+
+  def show
+    @user = current_user
+    @establishment = Establishment.find(params[:id])
+  end
+
+  def new
+    @user = current_user
+    @establishment = Establishment.new
+
+  def create
+
+  end
+
+end
+
+ private
+
+ def establishment_params
+  params.require(:establishment).permit(:name, :phone, :address, :type, :rating, :image, :photo)
+ end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_04_010533) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_05_093049) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,10 +19,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_04_010533) do
     t.string "address"
     t.integer "phone"
     t.string "name"
-    t.string "type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image"
+    t.integer "type", default: 0
   end
 
   create_table "favorites", force: :cascade do |t|
