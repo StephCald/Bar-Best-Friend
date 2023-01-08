01b1fcf3d884b3e3ca72da85aa8d60d1c5b5fb5d
Emily Lam <emilymeiyilam@gmail.com>
2023-01-08 18:41:39 +1100

    created admin permission for update and destroy...see comments

--- app/controllers/establishments_controller.rb
+++ app/controllers/establishments_controller.rb
@@ -1,20 +1,24 @@
 class EstablishmentsController < ApplicationController
+  before_action :set_establishment, only: %i[show edit update destroy]
+
   def index
     @user = current_user
-    @establishments = Establishment.all
+    @establishments = policy_scope(Establishment).all
   end
 
   def show
+    authorize @establishment
     @user = current_user
-    @establishment = Establishment.find(params[:id])
   end
 
   def new
+    authorize @establishment
     @user = current_user
     @establishment = Establishment.new
   end
 
   def create
+    authorize @establishment
     @establishment = Establishment.new(establishment_params)
     if @establishment.save
       redirect_to establishment_path(@establishment)
@@ -22,7 +26,7 @@ class EstablishmentsController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
-  
+
   # def favorite
   #   @establishment = Establishment.find(params[:id])
   #   current_user.favorite(@establishment)
@@ -35,7 +39,32 @@ class EstablishmentsController < ApplicationController
   #   redirect_to :back
   # end
 
+  def edit
+    authorize @establishment
+  end
+
+  def update
+    authorize @establishment
+    @establishment.update(establishment_params)
+    if @establishment.update(establishment_params)
+      redirect_to establishment_path(@establishment)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    authorize @establishment
+    @establishment.destroy
+    redirect_to root_path, status: :see_other
+  end
+
+
   private
+
+  def set_establishment
+    @establishment = Establishment.find(params[:id])
+  end
 
   def establishment_params
    params.require(:establishment).permit(:name, :phone, :address, :type, :rating, :image)
