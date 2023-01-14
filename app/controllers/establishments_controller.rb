869ba2564d007f428119a727fa499b0a0d679a49
Emily Lam <emilymeiyilam@gmail.com>
2023-01-14 22:45:06 +1100

    missing triangular bracket made the index look weird

--- app/controllers/establishments_controller.rb
+++ app/controllers/establishments_controller.rb
@@ -35,7 +35,7 @@ class EstablishmentsController < ApplicationController
     authorize @establishment
     if @establishment.save!
       redirect_to establishment_path(@establishment)
-    else
+    elsedevde
       render :new, status: :unprocessable_entity
     end
   end
