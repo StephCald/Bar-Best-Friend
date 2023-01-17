3fac3b617815f63df053ca9937c8c6358572eebd
benjamin treglown <treggy89@gmail.com>
2023-01-17 21:07:57 +1100

    collasing hamburger navbar with logo now set to home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, only: %i[home toggle_map]
 
   def home
     @user = current_user
